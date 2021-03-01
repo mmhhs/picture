cdb061b0fe3d4bc46468748fdafc4cbc7a5143c0
mmh <mmh199@163.COM>
2021-03-01 15:32:20 +0800

    修改重复点击问题

--- app/src/main/java/com/little/picture/PictureTakeActivity.java
+++ app/src/main/java/com/little/picture/PictureTakeActivity.java
@@ -99,10 +99,13 @@ public class PictureTakeActivity extends AppCompatActivity implements SurfaceHol
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);// 隐藏标题
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
+        try{
+            requestWindowFeature(Window.FEATURE_NO_TITLE);// 隐藏标题
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         setContentView(R.layout.activity_picture_take);
         bindView();
